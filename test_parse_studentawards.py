8e9ad7cb1c8bf5f7c8ab977249cef0e1c5771433
Matteo <fotimatteo71@gmail.com>
2026-03-07 17:05:59 -0500

    Cleanup redundant scrapers and temporary files

--- test_parse_studentawards.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup
-
-html = open('studentawards_utf8.txt', encoding='utf-8').read()
-soup = BeautifulSoup(html, 'html.parser')
-cards = soup.find_all('article')
-print(f'Found {len(cards)} articles')
-for c in cards:
-    print(c.text.strip()[:100])
